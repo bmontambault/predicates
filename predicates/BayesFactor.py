e39a810fba23b25a8d4324f3f90b013f9cf0159e
bmontambault <brianmontambault@gmail.com>
2023-02-27 22:43:36 -0500

    added JZS

--- predicates/BayesFactor.py
+++ predicates/BayesFactor.py
@@ -1,48 +1,69 @@
-import pandas as pd
 import numpy as np
 import scipy.stats as st
+from rpy2.robjects import Formula
+from rpy2.robjects.packages import importr
+from rpy2.robjects import numpy2ri, pandas2ri
+import rpy2.rinterface_lib.callbacks
+import rpy2.robjects as robjects
+
+numpy2ri.activate()
+pandas2ri.activate()
+RBayesFactor=importr('BayesFactor', suppress_messages=True)
 
 class BayesFactor(object):
     
     def __call__(self, value, mask1, mask2=None, *args, **kwargs):
         mask2 = ~mask1 if mask2 is None else mask2
         return self.bayes_factor(value[mask1], value[mask2], *args, **kwargs)
-
+    
+class JZS(BayesFactor):
+    
+    def __init__(self, side=None):
+        self.side = side
+    
+    def bayes_factor(self, value1, value2, return_samples=False):
+        if self.side == 'right' and value1.mean() < value2.mean():
+            bf = -np.inf
+        elif self.side == 'left' and value1.mean() < value2.mean():
+            bf = -np.inf
+        elif len(value1)>1 and len(value2)>2:
+            res = RBayesFactor.ttestBF(x=value1, y=value2)
+            bf = res.slots['bayesFactor']['bf'][0]
+        else:
+            bf = -np.inf
+        if return_samples:
+            return bf, (None, None, None)
+        else:
+            return bf
+    
 class Anomaly(BayesFactor):
     
     def __init__(self, num_samples=1000):
         self.num_samples = num_samples
     
-    def bayes_factor_(self, value1, value2, return_samples=False):
-        alpha = value2.count()
-        beta = value2.sum()
-        norm_samples = pd.Series(np.random.gamma(beta, 1/alpha, size=self.num_samples))
-        
-        mean = value1.mean()
-        prec = value1.count()*(1/value1.var())
-        std = np.sqrt(1./prec)
-        anom_samples = pd.Series(np.random.normal(mean, std, size=self.num_samples))
-        p = (anom_samples>norm_samples).mean()
-        if p == 1:
-            bf = np.inf
+    def bayes_factor(self, value1, value2, return_dist=False):
+        if len(value1) == 0:
+            bf = -np.inf
+            mean = 0
+            std = 1
+            mean_ = 0
+            prec_ = 1
+            dist = None
         else:
-            bf = p/(1-p)
-        if return_samples:
-            return bf, norm_samples, anom_samples
+            a = value2.count()
+            b = value2.sum()
+            mean = b/a
+            std = np.sqrt(b)/a
+
+            prec = 100
+            prec_ = prec*value1.count()
+            mean_ = value1.mean()
+
+            dist = st.norm(mean_ - mean, std + np.sqrt(1/prec_))
+            l1 = dist.logsf(0)
+            l2 = dist.logcdf(0)
+            bf = l1 - l2
+        if return_dist:
+            return bf, (st.norm(mean, std), st.norm(mean_, np.sqrt(1/prec_)), dist)
         else:
             return bf
-        
-    def bayes_factor(self, value1, value2):
-        alpha = value2.count()
-        beta = value2.sum()
-        mean_ = beta/alpha
-        std_ = beta/alpha**2
-        
-        mean = value1.mean()
-        prec = value1.count()*(1/value1.var())
-        std = np.sqrt(1./prec)
-        
-        dist = st.norm(mean - mean_, std + std_)
-        l1 = dist.logsf(0)
-        l2 = np.log(1-np.exp(l1))
-        return l1 - l2
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='predicates',
-	version='0.0.4',
+	version='0.0.5',
 	packages=['predicates'],
 	install_requires=[],
 )
